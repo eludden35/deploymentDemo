59888a9e19402d6d7783e415b61b6d6a34dce586
eludden35 <70604619+eludden35@users.noreply.github.com>
2020-10-27 15:10:59 -0500

    Update settings.py

--- tripBud_Proj/settings.py
+++ tripBud_Proj/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'etyl3g$jxlx(8jp2bd0+s#8x4o_t1uv7m_(j%wtrmbk^e9m9tj'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
